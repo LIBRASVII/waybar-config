a5ac64e9c87d6d7124d22bbabe02472deebc42cb
librasvii <moreiralucas511@gmail.com>
2024-01-21 11:49:16 -0300

    update config and add file

---
+++ get_media.sh
@@ -0,0 +1,14 @@
+media=$(playerctl metada -f "({{playername}} {{artist}} - {{title}})")
+player_status=$(playerctl status)
+
+if [[ $player_status = "Playing" ]]
+then
+    song_status=''
+elif [[ $player_status = "Paused" ]]
+then
+    song_status=''
+else
+    song_status'Music Stopped'
+fi
+
+echo -e "$song_status $media"
